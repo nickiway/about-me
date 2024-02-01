63798090e8ecae407eb033dc25b98bfea163d18b
nickiway <nickiwayua@gmail.com>
2024-02-01 16:49:48 +0100

    design fix

--- src/components/About/ContactButtons.tsx
+++ src/components/About/ContactButtons.tsx
@@ -5,17 +5,17 @@ import resume from "../../assets/resume-v3.pdf";
 
 const ContactButtons = () => {
   return (
-    <>
-      <NavLink to={"../contact"} className="primary-btn mx-5">
+    <div className="flex flex-wrap">
+      <NavLink to={"../contact"} className="primary-btn m-5">
         <Send />
         <span className="px-2">Contact Me</span>
       </NavLink>
 
-      <a href={resume} download={"NICK_RESUME"} className="primary-btn mx-5">
+      <a href={resume} download={"NICK_RESUME"} className="primary-btn m-5">
         <DocumentScanner />
         <span className="px-2">My CV</span>
       </a>
-    </>
+    </div>
   );
 };
 
